722ae87009866f425c0bc8e2fb0ade841955e80c
Lyxuz <151886396+EDiaz210@users.noreply.github.com>
2025-12-23 15:11:20 -0500

    Update JWT.js

--- src/middlewares/JWT.js
+++ src/middlewares/JWT.js
@@ -3,7 +3,7 @@ import User from "../models/User.js"
 
 const crearTokenJWT = (id, rol) => {
 
-    return jwt.sign({ id, rol }, process.env.JWT_SECRET, { expiresIn: "2m" })
+    return jwt.sign({ id, rol }, process.env.JWT_SECRET, { expiresIn: "1d" })
 }
 
 const verificarTokenJWT = async (req, res, next) => {
